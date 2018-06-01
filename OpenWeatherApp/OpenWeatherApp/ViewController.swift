4d555f462982b43589c12ec633b89fc785e2ca7f
Andrews, John L <John.L.Andrews@disney.com>
2018-06-01 15:42:02 -0700

    add project

---
+++ OpenWeatherApp/OpenWeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenWeatherApp
+//
+//  Created by Andrews, John L. on 6/1/18.
+//  Copyright © 2018 OpenWeatherApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
