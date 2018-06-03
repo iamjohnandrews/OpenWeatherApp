877e475033b49bd873a07e492570298a1392a5df
Andrews, John L <John.L.Andrews@disney.com>
2018-06-03 15:36:42 -0700

    create model objects for Weather data

--- OpenWeatherApp/OpenWeatherApp/Model/WeatherModels.swift
+++ OpenWeatherApp/OpenWeatherApp/Model/WeatherModels.swift
@@ -7,3 +7,61 @@
 //
 
 import Foundation
+
+
+struct Temperature {
+  var temp: Float 
+  var tempMin: Float
+  var tempMax: Float
+  var pressure: Float
+  var seaLevel: Float
+  var groundLevel: Float
+  var humidity: Float
+  var kf: Float
+  
+  init(temp: Float,
+       tempMin: Float,
+       tempMax: Float,
+       pressure: Float,
+       seaLevel: Float,
+       groundLevel: Float,
+       humidity: Float,
+       kf: Float
+    ) {
+    self.temp = temp
+    self.tempMin = tempMin
+    self.tempMax = tempMax
+    self.pressure = pressure
+    self.seaLevel = seaLevel
+    self.groundLevel = groundLevel
+    self.humidity = humidity
+    self.kf = kf
+  }
+}
+
+struct Weather {
+  let id: Int
+  var main: String
+  var description: String
+  var icon: String
+  
+  init(id: Int,
+       main: String,
+       description: String,
+       icon: String) {
+    self.id = id
+    self.main = main
+    self.description = description
+    self.icon = icon
+  }
+}
+
+struct Wind {
+  var speed: Float
+  var degrees: Float
+  
+  init(speed: Float, degrees: Float) {
+    self.speed = speed
+    self.degrees = degrees
+  }
+}
