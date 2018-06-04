dcabd632d3f73c76814b3b4bdf820d10ecc6a4ef
Andrews, John L <John.L.Andrews@disney.com>
2018-06-04 08:41:03 -0700

    hookup tableView

---
+++ OpenWeatherApp/OpenWeatherApp/View/WeatherCell.swift
@@ -0,0 +1,38 @@
+//
+//  WeatherCell.swift
+//  OpenWeatherApp
+//
+//  Created by Andrews, John L. on 6/4/18.
+//  Copyright © 2018 OpenWeatherApp. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+  @IBOutlet weak var temperature: UILabel!
+  @IBOutlet weak var overview: UILabel!
+  
+  @IBOutlet weak var maxTemp: UILabel!
+  @IBOutlet weak var minTemp: UILabel!
+  @IBOutlet weak var humidity: UILabel!
+  @IBOutlet weak var windSpeed: UILabel!
+  
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+  
+  func setContent(with weatherData: WeatherData) {
+    print("WeatherCell incoming data: \(weatherData)")
+    temperature.text = String(weatherData.1.temp)
+    maxTemp.text = String(weatherData.1.tempMax)
+    minTemp.text = String(weatherData.1.tempMin)
+    humidity.text = String(weatherData.1.humidity)
+    overview.text = String(weatherData.0.description)
+    windSpeed.text = String(weatherData.2.speed)
+  }
+}
--- OpenWeatherApp/OpenWeatherApp/ViewController.swift
+++ OpenWeatherApp/OpenWeatherApp/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import CoreLocation
 
 typealias JSONdata = [String: AnyObject]
+typealias WeatherData = (Weather, Temperature, Wind)
 
 class ViewController: UIViewController {
   let sharedSession = URLSession.shared
@@ -18,6 +19,7 @@ class ViewController: UIViewController {
   typealias Location = (Float, Float)
   
   @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var spinner: UIActivityIndicatorView!
   
   enum RequestType: String {
     case currentWeather = "weather"
@@ -30,14 +32,13 @@ class ViewController: UIViewController {
   }
   
   let sectionHeaderHeight: CGFloat = 30
-  typealias WeatherData = (Weather, Temperature, Wind)
   var weatherDataArray = [WeatherData]()
   var weatherDataDict = [TableSection: [WeatherData]]()
   
   var currentLocation: Location? {
     didSet {
       guard oldValue?.0 != currentLocation?.0 && oldValue?.1 != currentLocation?.1  else { return }
-
+      
       // When user's location changes, automatically make request for both current weather and five day forecast
       getWeather(for: .currentWeather)
       getWeather(for: .fiveDayForecast)
@@ -64,8 +65,13 @@ class ViewController: UIViewController {
     setupCoreLocation()
     setupTableView()
   }
-
-
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    spinner.startAnimating()
+    spinner.hidesWhenStopped = true
+  }
+  
   func getWeather(for type: RequestType) {
     guard let location = currentLocation, let url = apiURL(type.rawValue, location.0, location.1) else {
       displayErrorMessage()
@@ -84,27 +90,44 @@ class ViewController: UIViewController {
   func parseJSON(_ data: Data, for type: RequestType) {
     do {
       let serializedData = try JSONSerialization.jsonObject(with: data) as! JSONdata
-      
-      for element in serializedData {
-        print("WTF is this: \(element)")
-        var weatherData: WeatherData
-        if let temperature = Temperature.retrieveTemperatureObject(from: serializedData),
-          let weather = Weather.retrieveWeatherObject(from: serializedData),
-          let wind = Wind.retrieveWindObject(from: serializedData) {
-          weatherData = WeatherData(weather, temperature, wind)
-        }
-        if type == .currentWeather {
-          weatherDataArray.insert(weatherData, at: 0)
-        } else {
-          weatherDataArray.append(weatherData)
-        }
-      }
-      
-      print("What did I pull: \(serializedData)")
-      
+      if type == .currentWeather {
+        createWeatherData(from: serializedData, for: type)
+      } else {
+        createForecastWeatherData(from: serializedData[Parse.list.rawValue] as! [AnyObject])
+      }      
     } catch let error {
       handle(error)
       print("WTF Parsing error: \(error.localizedDescription)")
+    }
+  }
+  private func createForecastWeatherData(from array: [AnyObject]) {
+    for element in array {
+      if let json = element as? JSONdata {
+        createWeatherData(from: json, for: .fiveDayForecast)
+      }
+    }
+    reloadTableviewOnMainThread()
+  }
+  
+  private func createWeatherData(from json: JSONdata, for type: RequestType) {
+    guard let temperature = Temperature.retrieveTemperatureObject(from: json),
+      let weather = Weather.retrieveWeatherObject(from: json),
+      let wind = Wind.retrieveWindObject(from: json) 
+      else { 
+        displayErrorMessage()
+        return 
+    }
+    let weatherData = WeatherData(weather, temperature, wind)
+    if type == .currentWeather {
+      weatherDataArray.insert(weatherData, at: 0)
+    } else {
+      weatherDataArray.append(weatherData)
+    }
+  }
+  private func reloadTableviewOnMainThread() {
+    DispatchQueue.main.async { [unowned self] in
+      self.tableView.reloadData()
+      self.spinner.stopAnimating()
     }
   }
   
@@ -114,6 +137,8 @@ class ViewController: UIViewController {
   
   func displayErrorMessage() {
     // TODO: Share error with user
+    print("Error: displayErrorMessage called")
+    print("Whats inside weatherDataArray: \(weatherDataArray)")
   }
   
   private func setupCoreLocation() {
@@ -151,12 +176,22 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     return TableSection.total.rawValue
   }
   
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return section == 0 ? 1 : weatherDataArray.count - 1
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 200.0
   }
   
-  func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-    var sectionTitle: String
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    var rows = 0
+    if section == 0 {
+      rows = 1
+    } else {
+      rows = weatherDataArray.isEmpty ? 0 : weatherDataArray.count - 1
+    }
+    return rows
+  }
+
+  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    var sectionTitle = ""
     
     if let tableSection = TableSection(rawValue: section) {
       switch tableSection {
@@ -170,30 +205,13 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
     return sectionTitle
   }
-  /*
-  func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-    let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.width, height: sectionHeaderHeight))
-    view.backgroundColor = UIColor.cyan
-    let label = UILabel(frame: CGRect(x: 15, y: 0, width: tableView.bounds.width - 30, height: sectionHeaderHeight))
-    label.font = UIFont.boldSystemFont(ofSize: 25)
-    label.textColor = UIColor.black
-    
-    if let tableSection = TableSection(rawValue: section) {
-      switch tableSection {
-      case .currentWeather:
-        label.text = "Current Weather"
-      case .fiveDayForecast:
-        label.text = "Five Day Forecast"
-      default:
-        label.text = ""
-      }
-    }
-    view.addSubview(label)
-    return view
-  }
-  */
+  
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    <#code#>
+    let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath) as! WeatherCell
+    if !weatherDataArray.isEmpty {
+      cell.setContent(with: weatherDataArray[indexPath.item])
+    }
+    return cell
   }
 }
 
